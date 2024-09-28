e417fd71ecf95382bf4490d76f5d4697767ce26e
Usman Shaukat Gill <usman.gill@tuni.fi>
2024-09-28 19:53:55 +0300

    add, update, view course

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const adminRouter = express.Router();  // Initialize the router
-const { adminModel, courseModel } = require('../db'); // Import the adminModel
 const jwt = require('jsonwebtoken');
+
+const { adminModel, courseModel } = require('../db'); // Import the adminModel
 const { JWT_ADMIN_PASSWORD } = require('../config');
 const { adminMiddleware } = require('../middleware/admin');
 
@@ -18,14 +19,14 @@ adminRouter.post('/signup', async (req, res) => {
         });
     }
     catch(err){
-        res.status(400).json({ message: 'User signup failed' });
+        res.status(400).json({ message: 'Admin signup failed' });
         return
     }
 
-    res.json({ message: 'User signup succeeded' });
+    res.json({ message: 'Admin signup succeeded' });
 });
 
-adminRouter.post('/signin', async (req, res) => {
+adminRouter.post('/signin', async (req, res) => { 
     const { email, password } = req.body;
     
     // ideally password should be hashed and then compared
@@ -36,10 +37,10 @@ adminRouter.post('/signin', async (req, res) => {
 
         // do cookie stuff here
 
-        res.json({ message: 'User Signed In', token: token });
+        res.json({ message: 'Admin Signed In', token: token });
     }
     else {
-        res.status(403).json({ message: 'User signin failed' });
+        res.status(403).json({ message: 'Admin signin failed' });
     }
 });
 
@@ -65,13 +66,44 @@ adminRouter.post('/course',adminMiddleware,  async (req, res) => {
 });
 
 // edit a course
-adminRouter.put('/course',adminMiddleware,  (req, res) => {
-    res.json({ message: 'Add new course' });
+adminRouter.put('/course',adminMiddleware,  async (req, res) => {
+    const adminId = req.userId
+    const { title, description, imageUrl,  price, courseId} = req.body;
+
+
+    // creating a web3 saas in 6 hours // youtube video
+    const course = await courseModel.updateOne({
+        _id: courseId,
+        creatorId: adminId
+    }, 
+    {
+        title: title,
+        description: description,
+        imageUrl: imageUrl,
+        price: price,
+        adminId: adminId
+    });
+
+
+    res.json({ 
+        message: 'Course Created',
+        courseId: course._id
+    }); 
 });
 
 // admin can get all the courses he created
-adminRouter.get('/course/bulk', (req, res) => {
-    res.json({ message: 'Add new course' });
+adminRouter.get('/course/bulk', adminMiddleware, async(req, res) => {
+    const adminId = req.userId;
+
+    const courses = await courseModel.find({
+        creatorId : adminId,
+    });
+
+
+    res.json({ 
+        message: 'All courses',
+        courses
+    }); 
 });
 
 module.exports = adminRouter;  // Export the adminRouter
